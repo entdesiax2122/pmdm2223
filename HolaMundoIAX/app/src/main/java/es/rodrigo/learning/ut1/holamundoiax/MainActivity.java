179854d4dc453deb7e4a3adf7cf6886e0723f101
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-09-24 17:29:12 +0200

    Más archivos HolaMundoIAX

--- HolaMundoIAX/app/src/main/java/es/rodrigo/learning/ut1/holamundoiax/MainActivity.java
+++ HolaMundoIAX/app/src/main/java/es/rodrigo/learning/ut1/holamundoiax/MainActivity.java
@@ -3,12 +3,17 @@ package es.rodrigo.learning.ut1.holamundoiax;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String msg = "Ciclo de vida - onCreate";
+        Log.d(TAG, msg);
         setContentView(R.layout.activity_main);
     }
 }
