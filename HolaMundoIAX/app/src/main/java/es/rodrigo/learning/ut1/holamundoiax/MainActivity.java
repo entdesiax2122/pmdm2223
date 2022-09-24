0eec9168d87f8bf2f3607150b79bc1813bb25460
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-09-25 00:23:13 +0200

    Cambios

--- HolaMundoIAX/app/src/main/java/es/rodrigo/learning/ut1/holamundoiax/MainActivity.java
+++ HolaMundoIAX/app/src/main/java/es/rodrigo/learning/ut1/holamundoiax/MainActivity.java
@@ -8,12 +8,71 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "MainActivity";
+    private String pilaLogs = "";
+    private EditText etLogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String msg = "Ciclo de vida - onCreate";
         Log.d(TAG, msg);
+
         setContentView(R.layout.activity_main);
+        etLogs = (EditText) findViewById(R.id.etLogs);
+        actualizaEtLogs(msg);
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        String msg = "Ciclo de vida - onRestart";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String msg = "Ciclo de vida - onStart";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String msg = "Ciclo de vida - onResume";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String msg = "Ciclo de vida - onPause";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        String msg = "Ciclo de vida - onStop";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String msg = "Ciclo de vida - onDestroy";
+        Log.d(TAG, msg);
+        actualizaEtLogs(msg);
+    }
+
+    private void actualizaEtLogs(String msg) {
+        pilaLogs += msg + "\n";
+        etLogs.setText(pilaLogs);
+    }
+
 }
