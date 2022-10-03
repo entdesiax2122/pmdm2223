24ff5472578cfa48bd2b05968c249c2f75649b3d
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-10-03 17:56:50 +0200

    Ejemplo2Actividades

---
+++ Ejemplo2Actividades/app/src/main/java/es/rodrigo/learning/ut2/ejemplo2actividades/MainActivity.java
@@ -0,0 +1,14 @@
+package es.rodrigo.learning.ut2.ejemplo2actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
