e965cfc180473b4581fdac01060c43803b4d4089
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-10-20 15:45:10 +0200

    ListView

--- EjemploListView/app/src/main/java/dam/segundo/ejemplolistview/MainActivity.java
+++ EjemploListView/app/src/main/java/dam/segundo/ejemplolistview/MainActivity.java
@@ -1,14 +1,41 @@
 package dam.segundo.ejemplolistview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private ListView lvOpciones;
+    private TextView tvMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvOpciones = (ListView) findViewById(R.id.lvOpciones);
+        tvMensaje = (TextView) findViewById(R.id.tvMensaje);
+
+        final String[] datos =
+                new String[]{"Elem1", "Elem2", "Elem3", "Elem4", "Elem5"};
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_2, datos);
+        lvOpciones.setAdapter(adaptador);
+
+
+        lvOpciones.setOnItemClickListener(
+            new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent,
+                                        android.view.View v, int position, long id) {
+                    tvMensaje.setText("Seleccionado: " + parent.getItemAtPosition(position));
+                }
+            }
+        );
+
+
     }
 }
