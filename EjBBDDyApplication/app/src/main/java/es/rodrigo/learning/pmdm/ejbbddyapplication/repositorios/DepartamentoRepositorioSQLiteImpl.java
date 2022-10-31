3a508e9b9c48defcfa8fdac36196376ee2da3258
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-10-31 16:28:04 +0100

    BBDDyApplication

--- EjBBDDyApplication/app/src/main/java/es/rodrigo/learning/pmdm/ejbbddyapplication/repositorios/DepartamentoRepositorioSQLiteImpl.java
+++ EjBBDDyApplication/app/src/main/java/es/rodrigo/learning/pmdm/ejbbddyapplication/repositorios/DepartamentoRepositorioSQLiteImpl.java
@@ -1,8 +1,10 @@
 package es.rodrigo.learning.pmdm.ejbbddyapplication.repositorios;
 
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import es.rodrigo.learning.pmdm.ejbbddyapplication.modelos.Departamento;
@@ -13,10 +15,16 @@ public class DepartamentoRepositorioSQLiteImpl implements DepartamentoRepositori
     public DepartamentoRepositorioSQLiteImpl(SQLiteDatabase db) {
         this.db = db;
         db.execSQL("CREATE TABLE IF NOT EXISTS departamentos(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL);");
+
+        db.execSQL("INSERT INTO departamentos(nombre) VALUES ('Matemáticas');");
+        db.execSQL("INSERT INTO departamentos(nombre) VALUES ('Lengua');");
+        db.execSQL("INSERT INTO departamentos(nombre) VALUES ('Biología');");
+        db.execSQL("INSERT INTO departamentos(nombre) VALUES ('Informática');");
     }
 
     @Override
     public Integer guardar(Departamento d) {
+
         return null;
     }
 
@@ -34,6 +42,17 @@ public class DepartamentoRepositorioSQLiteImpl implements DepartamentoRepositori
 
     @Override
     public ArrayList<Departamento> recuperarTodos() {
-        return null;
+        ArrayList<Departamento> lista = new ArrayList<>();
+        Cursor c = db.rawQuery("SELECT * FROM departamentos", null);
+
+        if (c.getCount()>0) {
+            while (c.moveToNext()) {
+                Departamento d = new Departamento(c.getString(1));
+                d.setId(c.getInt(0));
+                lista.add(d);
+            }
+        }
+        c.close();
+        return lista;
     }
 }
