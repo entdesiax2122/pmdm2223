ed04da025f31cc43920148b1804b7a3df48b1f10
Rodrigo Martínez <rod.mart.rod@gmail.com>
2022-11-08 20:00:01 +0100

    Ejemplo Dialogos

--- EjemploDialogos/app/src/main/java/es/rodrigo/learning/pmdm/ejemplodialogos/MainActivity.java
+++ EjemploDialogos/app/src/main/java/es/rodrigo/learning/pmdm/ejemplodialogos/MainActivity.java
@@ -6,9 +6,11 @@ import android.view.View;
 import android.widget.Button;
 
 import es.rodrigo.learning.pmdm.ejemplodialogos.dialogs.SimpleInfoDialog;
+import es.rodrigo.learning.pmdm.ejemplodialogos.dialogs.SimpleInfoOkBtnDialog;
 
 public class MainActivity extends Activity {
     private Button btnSimpleInfo;
+    private Button btnSimpleInfoOkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class MainActivity extends Activity {
 
         // bindear componentes
         btnSimpleInfo = findViewById(R.id.btnSimpleInfo);
+        btnSimpleInfoOkBtn = findViewById(R.id.btnSimpleInfoOkBtn);
+
         btnSimpleInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SimpleInfoDialog sid = new SimpleInfoDialog();
-                sid.show(getFragmentManager(),"Mi diálogo");
+                sid.show(getFragmentManager(),"Mi diálogo simple");
+            }
+        });
+
+        btnSimpleInfoOkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SimpleInfoOkBtnDialog siobd = new SimpleInfoOkBtnDialog();
+                siobd.show(getFragmentManager(), "Mi dialogo simple con botón aceptar");
             }
         });
     }
