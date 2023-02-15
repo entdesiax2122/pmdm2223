bc902760f5ba8e75d777f54800e291f86259c61f
Rodrigo <entdesiax2122@gmail.com>
2023-02-15 13:16:38 +0100

    Captura y guarda en external

--- CapturaFoto/app/src/main/java/es/rodrigo/learning/pmdm/capturafoto/MainActivity.java
+++ CapturaFoto/app/src/main/java/es/rodrigo/learning/pmdm/capturafoto/MainActivity.java
@@ -5,22 +5,37 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import es.rodrigo.learning.pmdm.capturafoto.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCapturaFoto;
+    private Button btnCapturaFotoyGuardar;
     private ImageView ivFoto;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int CAPTURA_IMAGEN_TAMAÑO_REAL = 2;
+    private String mCurrentPhotoPath;
+
     ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
@@ -44,10 +59,26 @@ public class MainActivity extends AppCompatActivity {
                     Bundle bundle = result.getData().getExtras();
                     Bitmap bitmap = (Bitmap) bundle.get("data");
                     ivFoto.setImageBitmap(bitmap);
-
                 }
             }
         });
+
+        btnCapturaFotoyGuardar = findViewById(R.id.btnCapturaFotoyGuardar);
+        btnCapturaFotoyGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pedirPermisosSiNoSeTienen();
+                dispatchTakePictureIntentAndSave();
+            }
+        });
+    }
+
+    static String[] PERMISOS = {Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private void pedirPermisosSiNoSeTienen() {
+        if (ActivityCompat.checkSelfPermission(this, PERMISOS[0]) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, PERMISOS[1]) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, PERMISOS, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     private void dispatchTakePictureIntent() {
@@ -60,6 +91,50 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No se encuentra ninguna app para captura de foto", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void galleryAddPic() {
+        Intent mediaScanlntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanlntent.setData(contentUri);
+        this.sendBroadcast(mediaScanlntent);
+    }
+
+
+
+    private void dispatchTakePictureIntentAndSave() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "Error creando el fichero", Toast.LENGTH_SHORT).show();
+                Log.e("Error creando fichero", "dispatchTakePictureIntentAndSave: ", ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+//                startActivityForResult(takePictureIntent, CAPTURA_IMAGEN_TAMAÑO_REAL);
+                activityResultLauncher.launch(takePictureIntent);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(imageFileName,".jpg", storageDir);
+        mCurrentPhotoPath = "file:" + imageFile.getAbsolutePath();
+        return imageFile;
+    }
+
 
 //    @Override
 //    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
